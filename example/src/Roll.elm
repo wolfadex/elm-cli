eadeb2de063cc093f69dc3227d738abaaaf5067c
Wolfgang Schuster <wolfgang.schuster@vendr.com>
2022-08-29 17:08:35 -0500

    Fix the roll example

--- example/src/Roll.elm
+++ example/src/Roll.elm
@@ -6,29 +6,17 @@ module Roll exposing (..)
 import IO exposing (IO)
 import IO.Random
 import Random
+import System.Environment
 
 
 {-| This is the entry point, you can think of it as `main` in normal Elm applications.
 -}
-program : IO.Process -> IO String ()
-program process =
-    let
-        highLimit =
-            parseHighValue process.argv
-                |> Maybe.withDefault 100
-    in
-    IO.Random.generate (Random.int 1 highLimit)
+program : IO String ()
+program =
+    System.Environment.getArgs
+        |> IO.map (List.head >> Maybe.andThen String.toInt >> Maybe.withDefault 100)
+        |> IO.andThen (\highLimit -> IO.Random.generate (Random.int 1 highLimit) |> IO.map (Tuple.pair highLimit))
         |> IO.andThen
-            (\randomNumber ->
+            (\( highLimit, randomNumber ) ->
                 IO.printLn (String.fromInt randomNumber ++ " (" ++ String.fromInt highLimit ++ ")")
             )
-
-
-parseHighValue : List String -> Maybe Int
-parseHighValue args =
-    case args of
-        [ _, str ] ->
-            String.toInt str
-
-        _ ->
-            Nothing
